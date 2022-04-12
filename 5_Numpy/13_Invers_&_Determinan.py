deb622dae71a8087fcb1005d7eec05b5baa3387e
mayadenawa <wahana.eka30@gmail.com>
2022-04-12 19:09:21 +0700

    Update Numpy eps. 13

--- 5_Numpy/13_Invers_&_Determinan.py
+++ 5_Numpy/13_Invers_&_Determinan.py
@@ -1,5 +1,26 @@
 import numpy as np
 
-a = np.array([1,3])
+a = np.array([(1,-1), (1,1)])
+print("Matrik a = \n", a)
+print("\n")
 
-print(a)
+
+# INVERS MATRIX
+a_invers = np.linalg.inv(a)
+print("Invers dari matrik a adalah = \n", a_invers)
+
+# Untuk membuktikan, a dikali a invers, hasilnya adalah matrik satuan
+
+a_x_ainvers = np.dot(a, a_invers)
+print("Hasil kali a dan a invers adalah =\n", a_x_ainvers)
+
+
+# DETERMINAN MATRIX
+det_a = np.linalg.det(a)
+print("\nDeterminan dari a adalah =\n", det_a)
+
+det_a_invers =np.linalg.det(a_invers)
+print("Determinan dari a invers adalah =\n", det_a_invers)
+
+
+#SELESAI*
