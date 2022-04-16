50eea68c19a5669084290faca31c7b6006f2cc8d
mayadenawa <wahana.eka30@gmail.com>
2022-04-16 17:56:30 +0700

    modified pandas eps. 5

--- 6_Pandas/5_Membalik_urutan_baris_&_kolom.py
+++ 6_Pandas/5_Membalik_urutan_baris_&_kolom.py
@@ -13,3 +13,19 @@ print(dataframe)
 
 
 # Membalik urutan kolom
+print("\nmembalik urutan kolom")
+print(dataframe.loc[:, ::-1])
+# .loc[:,::-1] maksudnya adalah:
+# :    ===> baris nya tetap
+# ::-1 ===> kolomnya dibalik dengan jarak satu(-1)
+
+
+# Membalik urutan baris
+print("\nmembalik urutan baris")
+print(dataframe.loc[::-1, :])
+# [::-1, 1]  bisa juga ditulis [::-1] saja. karena yg dibalik urutannya hanya barisnya saja
+
+
+# Membalik urutan baris, tetapi index nya tetap
+print("\nmembalik urutan baris dengan index tetap")
+print(dataframe.loc[::-1].reset_index(drop =True))
