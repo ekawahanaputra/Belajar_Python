28760f8bc0efc0f5853b550cce41dd21b6a87d38
mayadenawa <wahana.eka30@gmail.com>
2022-04-22 22:15:30 +0700

    menambahkan materi matplotlib eps.1

---
+++ 7_Matplotlib/1_plot.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Tahapan membuat
+# 1. membuat data
+
+x = np.array([1,2,3,4])
+y = np.array([1,4,9,16])
+
+
+# 2. membuat plot
+plt.plot(x,y)
+
+
+# 3. menampilkan plot
+plt.show()
+
+# Selesai
